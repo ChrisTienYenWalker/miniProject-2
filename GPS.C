9d4dbe5fccc1658bcab7fd65b538fc668a88cd59
ChrisTienYenWalker <christienyenwalker@gmail.com>
2022-11-23 16:15:13 -0500

    added typedef

--- GPS.C
+++ GPS.C
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#define _OPEN_SYS_ITOA_EXT
+
+int *read(char *, char**);
+
+typedef struct user_t {
+    char name[50];
+    double time;
+    double latitude;
+    double longitude;
+    double altitude;
+};
 
 int main(int arguments, char* inputs[]){
-    printf("%s", inputs[1]);
+    //makes sure that the user enters the right data
+    if(arguments >= 5){
+        //check if the first input is a character
+        if(inputs[1][0] >40 && inputs[1][0] < 122){
+            
+        }
+    }
 }
+
+
