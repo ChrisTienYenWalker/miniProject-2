5330fef2bf5911a3998937fb4c9f9b61a27ca8b4
ChrisTienYenWalker <christienyenwalker@gmail.com>
2022-11-27 16:52:25 -0500

    differences made

--- GPS.C
+++ GPS.C
@@ -20,7 +20,7 @@ int main(int arguments, char *inputs[])
 {
     //inputs should be filename, name, time, longitude, latitude, altitude
     // makes sure that the user enters the right data
-    if (arguments >= 5)
+    if (arguments >= 6)
     {
         // check if the first input is a character
         if (inputs[1][0] > 40 && inputs[1][0] < 122)
@@ -41,12 +41,13 @@ int main(int arguments, char *inputs[])
             int counter = -1;
             const char *name;
             struct user_t other_users[50];
-            double time, logitude, latitude, altitude;
+            double time, longitude, latitude, altitude;
             int arraypositionCounter = 0;
+            int numberOfUsers;
             while (fgets(text, 20, txtfile))
             {
                 if(counter == -1){
-                    int numberOfUsers = atoi(text);
+                    numberOfUsers = atoi(text);
                     counter++;
                     continue;
                 }
@@ -60,7 +61,7 @@ int main(int arguments, char *inputs[])
                         // printf("%f", time);
                         break;
                     case 2:
-                        logitude = strtod(text, &ptr);
+                        longitude = strtod(text, &ptr);
                         break;
                     case 3:
                         latitude = strtod(text, &ptr);
@@ -69,78 +70,21 @@ int main(int arguments, char *inputs[])
                         altitude = strtod(text, &ptr);
                         counter = -1;
                         // printf("%f", time);
-                        other_users[arraypositionCounter] = {altitude, time, logitude, latitude, name};
+                        other_users[arraypositionCounter] = {altitude, time, longitude, latitude, name};
                         arraypositionCounter++;
                         
                         break;
                 }
                 counter++;
             }
-
+            
+            struct user_t differences[50];
+            for(int i = 0; i<numberOfUsers; i++){
+                differences[i] = {our_user.altitude-other_users[i].altitude, our_user.time-other_users[i].time, our_user.longitude-other_users[i].longitude, our_user.latitude-other_users[i].latitude, (char*) other_users[i].name};
+            }
             fclose(txtfile);
-            // int numberOfEnteries = 0;
-            // int digits = 0;
-            // for(int i = 0; i < strlen(text); i++){
-            //     if(text[i] == '\n'){
-            //         break;
-            //     }
-            //     numberOfEnteries = numberOfEnteries*10 + (int) text[i]-'0';
-            //     digits++;
-            // }
-            // printf("%d", numberOfEnteries);
 
-            // struct user_t other_users[50];
-            // for(int i = 0; i < numberOfEnteries; i++){
-            //     other_users[i] = {"", 0, 0, 0, 0};
-            // }
-            // char *textArray[250][16];
-            // int textArraycounter = 0;
-            // digits++;
-            // int referencePosition = text[digits];
-            // int positionInUserArray = 0;
-            // for(int i = digits; i < strlen(text); i++){
-            //     if(text[i] == '\n'){
-            //         char copiedText[50] = "15.02";
-            //         // strncpy(copiedText,&text[referencePosition],i - referencePosition);                                            printf("hi");
-            //                                 printf("hi");
-
-            //         if((i-digits)%5 == 0){
-            //             other_users[positionInUserArray].altitude = atoi(copiedText);
-            //             continue;
-            //         }
-            //         else if((i-digits)%4 == 0){
-            //             other_users[positionInUserArray].latitude = atoi(copiedText);
-            //             continue;
-            //         }
-            //         else if((i-digits)%3 == 0){
-            //             other_users[positionInUserArray].longitude =atoi(copiedText);
-            //             continue;
-            //         }
-            //         else if((i-digits)%2 == 0){
-            //             other_users[positionInUserArray].time =15;
-            //             continue;
-            //         }
-            //         else if((i-digits)%1 == 0){
-            //             // other_users[positionInUserArray].name = copiedText;
-            //             continue;
-            //         }
-            //         textArraycounter++;
-            //         referencePosition = i + 1;
-
-            //     }
-            // }
-
-            //     printf("\n%d\n\n", other_users[1].time);
         }
     }
 }
 
-char *read(char *fileName)
-{
-    FILE *txtfile;
-    char *text;
-    long bytes;
-    // open and read file
-
-    return text;
-}
